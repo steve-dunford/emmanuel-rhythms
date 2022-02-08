d561a65506686de758d140a823ac1debad71ad46
Steve Dunford <stephen@formalytics.io>
2022-02-08 21:08:24 +0000

    Initial projects

---
+++ emmanuel_rhythms_app/android/app/src/main/kotlin/com/emmanuel/rhythms/emmanuel_rhythms_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.emmanuel.rhythms.emmanuel_rhythms_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ emmanuel_rhythms_cms/android/app/src/main/java/com/emmanuel/rhythms/emmanuel_rhythms_cms/MainActivity.java
@@ -0,0 +1,6 @@
+package com.emmanuel.rhythms.emmanuel_rhythms_cms;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
