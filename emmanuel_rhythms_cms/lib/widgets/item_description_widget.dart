c8ef389e859d07969b0053aea9755f820de5c2c5
Steve Dunford <stephen@formalytics.io>
2023-02-15 22:22:49 +0000

    CMS: Custom fonts in description

--- emmanuel_rhythms_cms/lib/widgets/item_description_widget.dart
+++ emmanuel_rhythms_cms/lib/widgets/item_description_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:html_editor_enhanced/html_editor.dart';
+import 'package:pointer_interceptor/pointer_interceptor.dart';
 
 import '../common/widgets/themed_button.dart';
 
@@ -14,10 +15,14 @@ class ItemDescriptionWidget extends StatefulWidget {
 }
 
 class _ItemDescriptionWidgetState extends State<ItemDescriptionWidget> {
-  final descriptionController = HtmlEditorController();
+  final descriptionController = HtmlEditorController(
+  );
+  String? _selectedFont = 'Avenir Next';
+  bool _hasSetDefaultFont = false;
 
   @override
   Widget build(BuildContext context) {
+
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
@@ -28,13 +33,99 @@ class _ItemDescriptionWidgetState extends State<ItemDescriptionWidget> {
               htmlEditorOptions: HtmlEditorOptions(
                 hint: 'Description',
                 initialText: widget.initialDescription,
+                filePath: "assets/editor_template.html"
               ),
               htmlToolbarOptions:
-              const HtmlToolbarOptions(
+              HtmlToolbarOptions(
                 toolbarType: ToolbarType.nativeGrid,
+                defaultToolbarButtons: const [
+                  StyleButtons(),
+                  ColorButtons(),
+                  FontSettingButtons(fontName: false),
+                  FontButtons(),
+                  ListButtons(),
+                  ParagraphButtons(),
+                  InsertButtons(),
+                  OtherButtons(),
+                ],
+                customToolbarButtons: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    height: kMinInteractiveDimension,
+                    decoration:
+                    BoxDecoration(
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        border: Border.all(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSurface
+                                .withOpacity(0.12))),
+                    child: CustomDropdownButtonHideUnderline(
+                      child: CustomDropdownButton<String>(
+                        menuMaxHeight: MediaQuery.of(context).size.height / 3,
+                        menuDirection: DropdownMenuDirection.down,
+                        items: [
+                          CustomDropdownMenuItem(
+                            value: 'Avenir Next',
+                            child: PointerInterceptor(
+                                child: const Text('Avenir Next',
+                                    style: TextStyle(
+                                      fontFamily: 'Avenir Next',
+                                    ))),
+                          ),
+                          CustomDropdownMenuItem(
+                            value: 'MontSerrat',
+                            child: PointerInterceptor(
+                                child: const Text('MontSerrat',
+                                    style: TextStyle(
+                                      fontFamily: 'MontSerrat',
+                                    ))),
+                          ),
+                          CustomDropdownMenuItem(
+                            value: 'Courier New',
+                            child: PointerInterceptor(
+                                child: const Text('Courier New',
+                                    style: TextStyle(fontFamily: 'Courier'))),
+                          ),
+                          CustomDropdownMenuItem(
+                            value: 'sans-serif',
+                            child: PointerInterceptor(
+                                child: const Text('Sans Serif',
+                                    style: TextStyle(fontFamily: 'sans-serif'))),
+                          ),
+                          CustomDropdownMenuItem(
+                            value: 'Times New Roman',
+                            child: PointerInterceptor(
+                                child: const Text('Times New Roman',
+                                    style: TextStyle(fontFamily: 'Times'))),
+                          ),
+                        ],
+                        value: _selectedFont,
+                        onChanged: (String? changed) async {
+                          if (changed != null) {
+                            descriptionController.execCommand('fontName', argument: changed);
+                            setState(() {
+                              _selectedFont = changed;
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                  )
+                ],
               ),
               callbacks: Callbacks(
-                  onChangeContent: widget.descriptionUpdated),
+                  onKeyDown: (_) {
+                    //Default font
+                    if(!_hasSetDefaultFont && widget.initialDescription == null) {
+                      descriptionController.execCommand('fontName', argument: _selectedFont);
+                      _hasSetDefaultFont = true;
+                    }
+                  },
+                  onChangeContent: (newContent) {
+
+                    widget.descriptionUpdated(newContent);
+                  }),
             ),
           ),
           const SizedBox(height: 20),
